6a483c4dc4be1929877f1ad9a3a44ee3133aa401
mujwaidk <johnnyzhang76@gmail.com>
2018-04-19 00:27:12 +0800

    Firstly, K line has been drawn.

--- primary learning/stockinfo.py
+++ primary learning/stockinfo.py
@@ -1,9 +1,36 @@
-import tushare as ts;
-print("Hello world! tushare version ", ts.__version__);
-while True :
-    stocknum = input("Please input stock number : ")
-    if(stocknum == "quit") :
-        break;
-    df=ts.get_hist_data(stocknum);
-    print("stock ", stocknum);
-    print(df);
+import tushare as ts
+import matplotlib.pyplot as mp_plt
+from matplotlib.pylab import date2num
+import matplotlib.finance as mpf
+import datetime
+print("Hello world! tushare version ", ts.__version__)
+#while True:
+if(True):
+    stocknum = '601166'
+    #stocknum = input("Please input stock number : ")
+    df=ts.get_hist_data(stocknum)
+
+    print("stock ", stocknum)
+    data_list = []
+
+    for dates, row in df.iterrows():
+        # 将时间转换为数字
+        date_time = datetime.datetime.strptime(dates, '%Y-%m-%d')
+        t = date2num(date_time)
+        open, high, low, close = row[:4]
+        datas = (t, open, high, low, close)
+        data_list.append(datas)
+
+    # 创建子图
+    fig, ax = mp_plt.subplots()
+    fig.subplots_adjust(bottom=0.2)
+    # 设置X轴刻度为日期时间
+    ax.xaxis_date()
+    mp_plt.xticks(rotation=45)
+    mp_plt.yticks()
+    mp_plt.title("stock K line ")
+    mp_plt.xlabel("date")
+    mp_plt.ylabel("price")
+    mpf.candlestick_ohlc(ax, data_list, width=1.5, colorup='r', colordown='green')
+    mp_plt.grid()
+    mp_plt.show()
